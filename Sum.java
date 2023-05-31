235ca807e04683e45db761c3a8dcf1ab48def92a
leandromacedosilva <iamfulldeveloper@gmail.com>
2023-05-31 20:57:42 -0300

    more code implements

--- Sum.java
+++ Sum.java
@@ -1,13 +1,28 @@
 
 import java.util.Sacnner;
 
-public class Sum{
+public class Sum {
 	public static void main(String[] args) {
-		int valueNote1;
-		int valueNote2;
-		int valueNote3;
-		int valueNote4;
+
+		double valueNote1;
+		double valueNote2;
+		double valueNote3;
+		double valueNote4;
+
 		Scanner input = new Scanner();
 
+		System.out.println("informe o primeiro valor: ");
+		valueNote1 = input.nextDouble();
+
+		System.out.println("informe o segundo valor: ");
+		valueNote2 = input.nextDouble();
+
+		System.out.println("informe o valor da terceira nota: ");
+		valueNote3 = input.nextDouble();
+
+		System.out.println("informe o quarto valor: ");
+		valueNote4 = input.nextDouble();
+
+		System.exit(0);
 	}
 }
