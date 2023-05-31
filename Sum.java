38e804af50534337ec3a644544ca904fcdc2a15c
leandromacedosilva <iamfulldeveloper@gmail.com>
2023-05-31 20:47:35 -0300

    initial code implement

---
+++ Sum.java
@@ -0,0 +1,13 @@
+
+import java.util.Sacnner;
+
+public class Sum{
+	public static void main(String[] args) {
+		int valueNote1;
+		int valueNote2;
+		int valueNote3;
+		int valueNote4;
+		Scanner input = new Scanner();
+
+	}
+}
